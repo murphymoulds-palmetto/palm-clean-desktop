e9f1a5261c3f1ea7bbfcf360fe737350a691e6cc
Murphy Moulds <murphy.moulds@palmetto.com>
2022-06-21 11:40:09 -0500

    new directory check and create

--- .palm/cmd_clean-desktop.py
+++ .palm/cmd_clean-desktop.py
@@ -1,4 +1,5 @@
 from distutils.command.config import config
+import enum
 import click
 from typing import Optional
 from pathlib import Path
@@ -20,10 +21,11 @@ def cli(ctx):
 def clean_desktop(ctx):
 
     src_path = Path.home() / 'Desktop'
+
     configuration = {
         'images': {
             'destination_path': '', 
-            'allowed_values': ['.jpg', '.png']
+            'allowed_values': ['.jpg', '.png', '.jpeg']
             },
         'spreadsheets': {
             'destination_path': '',
@@ -35,6 +37,9 @@ def clean_desktop(ctx):
     for i, (key, val) in enumerate(configuration.items()):
         destination_path = Path(src_path / key)
         configuration[key]['destination_path'] = destination_path
+
+        # create the directories if they don't exist
+        destination_path.mkdir(parents=True, exist_ok=True)
 
         # grabs all files
         for each_file in Path(src_path).glob('*.*'): 
