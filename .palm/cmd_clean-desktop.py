cd22e5f5b328c6576ac68f4652a61235c0400cac
Murphy Moulds <murphy.moulds@palmetto.com>
2022-06-28 22:47:22 -0500

    secho and testing

--- .palm/cmd_clean-desktop.py
+++ .palm/cmd_clean-desktop.py
@@ -1,4 +1,3 @@
-from bdb import Breakpoint
 from distutils.command.config import config
 import enum
 import click
@@ -24,22 +23,11 @@ def clean_desktop(ctx):
 
     src_path = Path.home() / 'Desktop'
     file_directory_config = Path(__file__).parent / 'file_directory_config.yaml'
-
-    configuration = {
-        'images': {
-            'destination_path': '', 
-            'allowed_values': ['.jpg', '.png', '.jpeg']
-            },
-        'spreadsheets': {
-            'destination_path': '',
-            'allowed_values': ['.csv', '.numbers']
-        }
-    }
+    unmatched_files = []
 
     with file_directory_config.open() as fp:
-        data = yaml.safe_load(fp)
+        configuration = yaml.safe_load(fp)
 
-    breakpoint()
     # get Path to destinations
     for i, (key, val) in enumerate(configuration.items()):
         destination_path = Path(src_path / key)
@@ -49,7 +37,16 @@ def clean_desktop(ctx):
         destination_path.mkdir(parents=True, exist_ok=True)
 
         # grabs all files
-        for each_file in Path(src_path).glob('*.*'): 
-            if each_file.suffix in configuration[key]['allowed_values']:
-                # moves to parent folder.
-                each_file.rename(destination_path.joinpath(each_file.name))
+        for each_file in Path(src_path).glob('*.*'):
+            if not each_file.name.startswith('.'):
+                if each_file.suffix in configuration[key]['allowed_values']:
+                    # moves to parent folder.
+                    each_file.rename(destination_path.joinpath(each_file.name))
+                else:
+                    if each_file.suffix not in unmatched_files:
+                        unmatched_files.append(each_file.suffix)
+
+    if len(unmatched_files) > 0:
+        click.secho(f"{len(unmatched_files)} suffixes did not have a designation:", fg="yellow")
+        for file in unmatched_files:
+            click.secho(f"{file}", fg="red")
