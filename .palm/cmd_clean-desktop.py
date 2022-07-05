d9c8939b648b1368c7ea7b09d80f4c6bc37b3d06
Murphy Moulds <murphy.moulds@palmetto.com>
2022-07-05 09:59:34 -0500

    made it workable

--- .palm/cmd_clean-desktop.py
+++ .palm/cmd_clean-desktop.py
@@ -23,8 +23,7 @@ def clean_desktop(ctx):
 
     src_path = Path.home() / 'Desktop'
     file_directory_config = Path(__file__).parent / 'file_directory_config.yaml'
-    unmatched_files = []
-
+    
     with file_directory_config.open() as fp:
         configuration = yaml.safe_load(fp)
 
@@ -39,14 +38,39 @@ def clean_desktop(ctx):
         # grabs all files
         for each_file in Path(src_path).glob('*.*'):
             if not each_file.name.startswith('.'):
-                if each_file.suffix in configuration[key]['allowed_values']:
-                    # moves to parent folder.
-                    each_file.rename(destination_path.joinpath(each_file.name))
-                else:
-                    if each_file.suffix not in unmatched_files:
-                        unmatched_files.append(each_file.suffix)
+                handle_unmatched_suffixes(ctx, each_file.suffix, file_directory_config)
+                each_file.rename(destination_path.joinpath(each_file.name))
 
-    if len(unmatched_files) > 0:
-        click.secho(f"{len(unmatched_files)} suffixes did not have a designation:", fg="yellow")
-        for file in unmatched_files:
-            click.secho(f"{file}", fg="red")
+def handle_unmatched_suffixes(ctx, suffix_to_check, file_directory_config):
+    
+    with file_directory_config.open() as fp:
+        configuration_raw = yaml.safe_load(fp)
+
+    configured_suffixes = []
+
+    template_config = {
+        'allowed_values': []
+    }
+
+    for key, val in configuration_raw.items():
+        configured_suffixes = configured_suffixes + val['allowed_values']
+
+    if suffix_to_check not in configured_suffixes:
+
+        click.secho(f"{suffix_to_check}", fg="red")
+        user_wants_update = click.Choice(choices=['yes', 'no'], case_sensitive=False)
+
+        if user_wants_update:
+
+            click.echo("Here is a current list of folders you have configured")
+            click.echo(f"{ configuration_raw.keys() }")
+            name_of_folder = click.prompt(f"Type name of folder to put {suffix_to_check} in.")
+
+            if name_of_folder not in list(configuration_raw.keys()):
+                configuration_raw[name_of_folder] = template_config
+
+            if suffix_to_check not in configuration_raw[name_of_folder]['allowed_values']:
+                configuration_raw[name_of_folder]['allowed_values'].append(suffix_to_check)
+
+        with file_directory_config.open('w') as fp:
+            yaml.safe_dump(configuration_raw, fp, indent=4, default_flow_style=False)
